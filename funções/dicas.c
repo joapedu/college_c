72d85523c20f33b03189ccf49c86927c5c049395
joaoeduardo <109396949+joapedu@users.noreply.github.com>
2022-09-26 19:53:41 -0300

    Create dicas.c

---
+++ funções/dicas.c
@@ -0,0 +1,23 @@
+//basicamente a função é
+//  void nome_da_função(variáveis que vou utilizar){
+//      o que eu quero de ação (if) (for) (else)
+//  }
+
+
+//bibliotecas
+
+#include <stdio.h>
+//  básica para entrada e saída de dados
+
+#include <math.h>
+//  para uso de funções matemáticas!!
+//  (ROUND) Arredonda o valor da seguinte forma: de 0-4 arredonda para baixo, de 5-9 arredonda para cima
+//  double ceil(double numero) — arredonda para CIMA
+//  double floor(double numero) — arredonda para BAIXO
+//  double pow(double x, double y) — função que faz a POTÊNCIA de x^y
+//  double sqrt(double x) — função que faz a RAIZ QUADRADA de x
+//  double abs(double x) — Função que retorna o valor absoluto (o módulo, o valor sem sinal) de um número
+
+#include <stdlib.h>
+//  int rand() — função que sorteia um valor “aleatório”
+//  void srand(int sem) — Função que alimenta uma semente para a função rand
